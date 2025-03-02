e829731b88b959d3aabb0702ae3be929e57b528a
Anugrg <grg.jomle@gmail.com>
2025-03-02 17:06:38 +0700

    udpates

--- nasfer.py
+++ nasfer.py
@@ -79,6 +79,7 @@ class ConfigComp:
 
 def match_dir_nas(conn, dir_name, service_name):
     exists = False
+    print(dir_name)
     for i in conn.listPath(service_name, '/'):
 
         if i.isDirectory and i.filename == dir_name:
@@ -94,11 +95,25 @@ def create_dir_nas(conn, service_name, dir):
      logger.error('Failed to create directory', exc_info=True)
 
 
+def get_full_paths(directory_dict):
+    full_paths = {}
+    for parent, child in directory_dict.items():
+        if parent in full_paths:
+            full_paths[child] = os.path.join(full_paths[parent], child).replace('\\', '/')
+        else:
+            full_paths[child] = os.path.join('/', parent, child).replace('\\', '/')
+    return full_paths
+
+
 def transfer_file_samba_style(*args):
+    print('transferring')
     file = args[0]
     target = args[1]
     client_machine_name = args[2]
-
+    file_paths = file.split('\\')
+    file_paths.pop(0)
+    file_paths.pop(0)
+    
     userID = target['USER_ID']
     password = target['PASSWORD']
     server_name = target['SERVER_NAME']
@@ -106,27 +121,49 @@ def transfer_file_samba_style(*args):
     nas_ip = target['NAS_IP']
     nas_port = target['NAS_PORT']
     new_name = uuid.uuid4()
-    _date = dt.today()
-    dir_name = _date.strftime('%Y_%m_%d')
-    hr_dir = _date.strftime('%H')
+    dests = target['DEST_FOLDER'].split('/')
+    print(dests)
+    directory_dict = {}
+    for i in range(len(dests) - 1):
+        directory_dict[dests[i]] = dests[i + 1]
 
+    name =  os.path.basename(file)
+    print(name)
+    full_paths = get_full_paths(directory_dict)
+    full_paths[dests[0]] = f'/{dests[0]}'
+    final_path = full_paths[dests[-1]] 
+    print(final_path)
+    print(full_paths)
+    
     conn = SMBConnection(userID, password, client_machine_name, server_name, use_ntlm_v2=True)
     assert conn.connect(nas_ip, nas_port)
-    exists = match_dir_nas(conn, dir_name, service_name)
+    for dest in dests:
+        if not match_dir_nas(conn, full_paths[dest], service_name):
+            create_dir_nas(conn, service_name, full_paths[dest])
 
-    # check if date directory exists
-    if not exists:
-        create_dir_nas(conn, service_name, dir_name)
+
+    # _date = dt.today()
+    # dir_name = _date.strftime('%Y_%m_%d')
+    # hr_dir = _date.strftime('%H')
+
+
+    # exists = match_dir_nas(conn, dir_name, service_name)
+
+    # # check if date directory exists
+    # if not exists:
+    #     create_dir_nas(conn, service_name, dir_name)
     
-    # check if hr directory exists
-    exists = match_dir_nas(conn, f'/{dir_name}/{hr_dir}', service_name)
+    # # check if hr directory exists
+    # exists = match_dir_nas(conn, f'/{dir_name}/{hr_dir}', service_name)
 
-    if not exists:
-        create_dir_nas(conn, service_name, f'{dir_name}/{hr_dir}')
-
+    # if not exists:
+    #     create_dir_nas(conn, service_name, f'{dir_name}/{hr_dir}')
+    name =  os.path.basename(file)
+    print(name) 
     try:
         with open(f'{file}', 'rb') as f:
-            conn.storeFile(service_name, f'/{dir_name}/{hr_dir}/{new_name}.png', f, show_progress=True)
+            #conn.storeFile(service_name, f'/{dir_name}/{hr_dir}/{new_name}.png', f, show_progress=True)
+            conn.storeFile(service_name, f'/{final_path}/{name}', f, show_progress=True)
 
     except Exception as e:
         logger.error('Failed to transfer', exc_info=True)
@@ -173,12 +210,13 @@ def check_latest_file(conf):
 
     status = False
     targets = conf.get("NAS_SERVERS")
+    pattern = conf.get('pattern')
     try:
         for target in targets:
             src = target['SRC_FOLDER']
             print(f'Searching in {src}')
             for file in os.listdir(target['SRC_FOLDER']):
-                if fnmatch.fnmatch(file, 'NEW*.png'):
+                if fnmatch.fnmatch(file, f'{pattern}.png'):
                     logger.info(f'New file discovered in {src} ... Transferring to NAS')
                     if conf.get('USE_SAMBA'):
                         logger.info('Using SAMBA protocol')
@@ -262,6 +300,8 @@ if __name__=='__main__':
             _settings = json.load(f)
     except Exception as e:
         logger.error("Can't open config json file", exc_info=True)
-
-    asyncio.run(run2(_settings))
+    if _settings.get('mode'):
+        asyncio.run(run2(_settings))
+    else:
+        asyncio.run(run(_settings))
 
